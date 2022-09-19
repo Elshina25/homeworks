81ed875e812f9b7a0bdaf629fd8e35d01a37066d
Анастасия Ельшина <salnaya92@gmail.com>
2022-09-19 12:45:21 +0300

    Task2

---
+++ task.js
@@ -0,0 +1,14 @@
+const clickCookie = document.getElementById('cookie');
+let countClick = 0;
+
+clickCookie.onclick = function() {
+    countClick++;
+    if (countClick % 2 !== 0) {
+        clickCookie.width = 250;
+    } else {
+        clickCookie.width = 200;
+    }
+    
+    document.getElementById('clicker__counter').textContent = countClick;
+    console.log('Click!');
+}
